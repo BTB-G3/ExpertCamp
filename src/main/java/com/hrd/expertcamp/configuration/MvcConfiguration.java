89e8ed59b01c6845a0141eaf278ab5b43063b55d
raksmey <raksmey111@gmail.com>
2017-07-16 10:13:28 +0700

    config mybatis

--- src/main/java/com/hrd/expertcamp/configuration/MvcConfiguration.java
+++ src/main/java/com/hrd/expertcamp/configuration/MvcConfiguration.java
@@ -1,10 +1,19 @@
 package com.hrd.expertcamp.configuration;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 //@EnableWebMvc
@@ -27,5 +36,51 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 	public void addViewControllers(ViewControllerRegistry registry) {
 		
 			registry.addViewController("/admin").setViewName("/admin/dashboard");
+//			registry.addViewController("/swagger").setViewName("/swagger/dist/lib/o2c");
+//			registry.addViewController("/swagger-ui.html").setViewName("/swagger/dist/lib/o2c");
+	}
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver resolver = new CookieLocaleResolver();
+		resolver.setDefaultLocale(new Locale("kh"));
+		resolver.setCookieName("Spring_Locale");
+		resolver.setCookieMaxAge(4800);
+		return resolver;
+	}
+
+	/*
+	 * Next, we need to add an interceptor bean that will switch to a new locale
+	 * based on the value of the lang parameter appended to a request:
+	 */
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	/*
+	 * In order to take effect, this bean needs to be added to the application’s
+	 * interceptor registry.
+	 *
+	 * To achieve this, our @Configuration class has to extend the
+	 * WebMvcConfigurerAdapter class and override the addInterceptors() method:
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	/*
+	 * Defining the Message Sources
+	 */
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames("classpath:/i18n/messages/message");
+		messageSource.setCacheSeconds(0);
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
 	}
 }
---
+++ src/main/java/com/hrd/expertcamp/configuration/SwaggerConfiguration.java
@@ -0,0 +1,40 @@
+package com.hrd.expertcamp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()    
+          .apis(RequestHandlerSelectors.basePackage("com.hrd.expertcamp"))
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build()
+	      .apiInfo(apiInfo());                                   
+    }
+    
+	
+	private ApiInfo apiInfo() {
+	  
+		ApiInfo apiInfo = new ApiInfo(
+	      "Spring RESTful Web Service",
+	      "Korea Software HRD Center.",
+	      "API Spring",
+	      "Terms of service",
+	      "Reksmey Mom - raksmeymom111@gmail.com",
+	      "License of API",
+	      "http://www.khmeracademy.org/");
+	    return apiInfo;
+	}
+}
